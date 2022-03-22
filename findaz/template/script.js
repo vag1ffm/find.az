9db275a4df52c2d523907b7be264673d232f54d4
vag1ffm <vaqif.mamedov.2002@gmail.com>
2022-03-22 13:20:38 +0400

    made filter)

---
+++ findaz/template/script.js
@@ -0,0 +1,24 @@
+function fshowfilter() {
+	let a = document.querySelector('.fsf')
+	a.classList.toggle('aaa')
+}
+
+function vshowfilter() {
+	let a = document.querySelector('.vsf')
+	a.classList.toggle('aaa')
+}
+
+function tshowfilter() {
+	let a = document.querySelector('.tsf')
+	a.classList.toggle('aaa')
+}
+
+function cshowfilter() {
+	let a = document.querySelector('.csf')
+	a.classList.toggle('aaa')
+}
+
+function pshowfilter() {
+	let a = document.querySelector('.psf')
+	a.classList.toggle('aaa')
+}
