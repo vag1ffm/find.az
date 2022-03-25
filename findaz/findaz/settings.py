45ec79dedff55103aa724076af3e41d6e843a6ff
Amir Hasanov <hasanov.amir.81@gmail.com>
2022-03-25 21:05:45 +0400

    stupid bug fix

--- findaz/findaz/settings.py
+++ findaz/findaz/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import os
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,7 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
