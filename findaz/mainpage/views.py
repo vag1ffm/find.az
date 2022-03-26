5e55b380f2852f81e41d11f64124996e5b869e83
Amir Hasanov <hasanov.amir.81@gmail.com>
2022-03-27 00:20:26 +0400

    added database models and connected it with html templates

--- findaz/mainpage/views.py
+++ findaz/mainpage/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Items
+
 
 def shop(request):
-    return render(request, 'mainpage/index.html')
+    items = Items.objects.all()
+    return render(request, 'mainpage/index.html', {"items": items})
