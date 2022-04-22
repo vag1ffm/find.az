d83806a51d12e1306107831a495db58cdd926e9d
Amir Hasanov <hasanov.amir.81@gmail.com>
2022-04-22 20:38:15 +0400

    aaaaaaa

--- findaz/mainpage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ShopMainPage.as_view(), name='MainPage'),
-]
--- findaz/mainpage/views.py
+++
@@ -1,9 +0,0 @@
-from .models import Items
-from django.views.generic import TemplateView
-
-
-class ShopMainPage(TemplateView):
-    model = Items
-    template_name = 'mainpage/index.html'
-    context_object_name = 'items'
-
